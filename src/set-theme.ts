53633f8f8400a2c3875787856055d74ef4b57a35
Wellington Grisa <wellington.grisa@gmail.com>
2022-03-29 20:18:03 -0300

    Include initial project structure

---
+++ src/set-theme.ts
@@ -0,0 +1,40 @@
+const setDocumentProperty = ({ name, value }) => document.documentElement.style.setProperty(`--${name}`, value)
+
+export interface ThemeColorCreation<TVariableName> extends ThemeAttribute<TVariableName> {
+  getHueSaturationAndLightness: GetHueSaturationAndLightnessType
+}
+
+export const createThemeAttribute = <TVariableName>({
+  name,
+  value,
+  getHueSaturationAndLightness,
+}: ThemeColorCreation<TVariableName>) => {
+  const { hue, saturation, lightness } = getHueSaturationAndLightness(value)
+
+  setDocumentProperty({ name: `${name}-h`, value: hue })
+  setDocumentProperty({ name: `${name}-s`, value: `${saturation}%` })
+  setDocumentProperty({ name: `${name}-l`, value: `${lightness}%` })
+}
+
+export const getDocumentBodyVariableByName = (name) => getComputedStyle(document.body).getPropertyValue(`--${name}`)
+
+export interface ThemeAttribute<TVariableName> {
+  name: TVariableName
+  value: string
+}
+
+export type GetHueSaturationAndLightnessType = (value: string) => {
+  hue: number
+  saturation: number
+  lightness: number
+}
+
+export interface Theme<TVariableName> {
+  variables: ThemeAttribute<TVariableName>[]
+  getHueSaturationAndLightness: GetHueSaturationAndLightnessType
+}
+
+export const setTheme = <TVariableName>({ variables, getHueSaturationAndLightness }: Partial<Theme<TVariableName>>) =>
+  variables.forEach(({ name, value }) => {
+    createThemeAttribute({ name, value, getHueSaturationAndLightness: getHueSaturationAndLightness })
+  })
